9513897e2bd777fe86cb771909ef42b45f6c1d29
rjavalane <rachid.alane@yahoo.fr>
2022-01-20 03:05:25 -0500

    definition package

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -15,7 +15,7 @@ public class LowestNumber {
 		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
 
 		int min = array [0];
-		int max = array [0];
+		//int max = array [0];
 
 		for (int i=0; i<array.length; i++){
 			if (array[i]<min){
--- src/math/problems/Pattern.java
+++ src/math/problems/Pattern.java
@@ -3,13 +3,30 @@ package math.problems;
 public class Pattern {
 
 	public static void main(String[] args) {
-		/* Read this numbers, find the pattern then implement the logic from this pattern.which will give you this output.
-		 * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32............
-		 *
-		 *
-		 */
-		
-
-
+		Pattern.pattern();
 	}
-}
+
+		public static int pattern () {
+			int i;
+
+			for (i = 100; i >= 1; i--) {
+				if (i >= 90 && i <= 100) {
+					i = i;
+				} else if (i <= 90 && i >= 70) {
+					i--;
+				} else if (i <= 70 && i >= 40) {
+					i -= 2;
+				} else {
+					i -= 3;
+				}
+				System.out.print(i+ ",");
+
+			}
+			return i;
+		}
+	}
+/* Read this numbers, find the pattern then implement the logic from this pattern.which will give you this output.
+ * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32............
+ *
+ *
+ */
