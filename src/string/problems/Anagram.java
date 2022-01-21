889d0b1e1f4c278232cf82c9079d784e324420f5
rjavalane <rachid.alane@yahoo.fr>
2022-01-21 18:01:56 -0500

    String_package

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -2,8 +2,31 @@ package string.problems;
 
 
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class Anagram {}
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first String: ");
+        String word1= sc.nextLine();
+        System.out.println("Enter the second String: ");
+        String word2= sc.nextLine();
+        if (word2.length()==word2.length()){
+            char [] array1=word1.toLowerCase().toCharArray();
+            char [] array2=word2.toLowerCase().toCharArray();
+            Arrays.sort(array1);
+            Arrays.sort(array2);
+            boolean result=Arrays.equals(array1,array2);
+            if(result) {
+                System.out.println(word1 + " and " + word2 + " are Anagram.");
+            }
+            else{
+                System.out.println(word1 + " and " + word2 + " are Not Anagram.");
+            }
+        }
+
+    }
+}
 
 
 //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -13,13 +13,23 @@ public class DetermineLargestWord {
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
 
+        for(Map.Entry entry:findTheLargestWord(s).entrySet()){
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
+
+
     }
 
     public static Map<Integer, String> findTheLargestWord(String wordGiven){
         Map<Integer, String> map = new HashMap<Integer, String>();
         String st = "";
-        //implement
-
+        String[] word=wordGiven.split(" ");
+        for (int i=0;i<word.length;i++){
+            if(word[i].length()>=st.length()){
+                st=word[i];
+            }
+        }
+        map.put(st.length(),st);
         return map;
     }
 }
