dd68ff6547f96b5e2910d1c5298680719e29dd20
rjavalane <rachid.alane@yahoo.fr>
2022-01-21 21:11:02 -0500

    math-package

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,46 +1,44 @@
 package math.problems;
 
+
 import java.util.Arrays;
 
 public class FindLowestDifference {
-    public static void main(String[] args) {
-
-        Integer [] array1 = {30,12,5,9,2,20,33,1};
-        Integer [] array2 = {18,25,41,47,17,36,14,19};
-
-
-        int minDiff =Integer.MAX_VALUE;
-        int i=0;
-        int j=0;
-        int min1=-1;
-        int min2=-1;
-        int diff=0;
+    public static int lowestDifference(int[] array1, int[] array2) {
+        int minDiff = Integer.MAX_VALUE;
+        int i = 0;
+        int j = 0;
+        int min1 = -1;
+        int min2 = -1;
+        int diff = 0;
         Arrays.sort(array1);
         Arrays.sort(array2);
-        while(i<array1.length && j< array2.length){
-
-            diff=Math.abs(array1[i]-array2[j]);
-
-            if(diff<minDiff){
-                minDiff=diff;
-                min1=array1[i];
-                min2=array2[j];
+        while (i < array1.length && j < array2.length) {
+            diff = Math.abs(array1[i] - array2[j]);
+            if (diff < minDiff) {
+                minDiff = diff;
+                min1 = array1[i];
+                min2 = array2[j];
             }
-
-            if(array1[i]<array2[j]){
+            if (array1[i] < array2[j]) {
                 i++;
-            }
-
-            else{
+            } else {
                 j++;
-
             }
         }
-        System.out.println("The lowest difference between the two arrays is the difference between " + min1 + " and "+min2+" and it equals: "+ minDiff);
+        return minDiff;
 
     }
 
+    public static void main(String[] args) {
+        int [] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
+        int [] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
+        int result=lowestDifference(array1,array2);
+        System.out.println(result);
+
+    }
 }
+
   /*
          Implement in java.
          Read this below two array. Find the lowest difference between the two array cell.
--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -3,34 +3,70 @@ package math.problems;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.awt.peer.SystemTrayPeer;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class UnitTestingMath {
 
 
-        @Test
-        public void testFactorial() {
-            int CurrentResult=Factorial.factorialRec(5);
-            int ExpectedResult=120;
-            Assert.assertEquals(CurrentResult, ExpectedResult);
-            System.out.println("Test factorial using recursion PASSED");
-
-        }
     @Test
-    public void testFactorial1() {
-        int CurrentResult=Factorial.factorialIter(5);
+    public void testfactorial(){
+        int currentResult=Factorial.factorialRec(5);
         int ExpectedResult=120;
-        Assert.assertEquals(CurrentResult, ExpectedResult);
-        System.out.println("Test factorial using iteration PASSED");
-
+        Assert.assertEquals(currentResult,ExpectedResult);
+        System.out.println(" passed ");
     }
     @Test
-    public void testFibonacci() {
-        int CurrentResult=Fibonacci.fibonacci(40);
-        int ExpectedResult=39088169+63245986;
-        Assert.assertEquals(CurrentResult, ExpectedResult);
-        System.out.println("Test factorial using iteration PASSED");
-
-   }
-
+    public void testfactorial1(){
+        int currentResult=Factorial.factorialIter(5);
+        int ExpectedResult=120;
+        Assert.assertEquals(currentResult,ExpectedResult);
+        System.out.println(" passed ");
     }
+    @Test
+    public void testFibonacci(){
+        int currentResult=Fibonacci.fibonacci(40);
+        int ExpectedResult=63245986+39088169;
+        Assert.assertEquals(currentResult,ExpectedResult);
+        System.out.println(" passed");
+    }
+    @Test
+    public void testFindLowestDifference(){
+        int [] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
+        int [] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
+        int currentResult=FindLowestDifference.lowestDifference(array1,array2);
+        int ExpectedResult=1;
+        Assert.assertEquals(currentResult,ExpectedResult);
+        System.out.println(" passed");
+    }
+    @Test
+    public void testMissingNumber(){
+        int[] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+        int currentResult=FindMissingNumber.missingNumber(array);
+        int ExpectedResult=9;
+        Assert.assertEquals(currentResult,ExpectedResult);
+        System.out.println(" passed");
+    }
+    @Test
+    public void testLowestNumber(){
+        int array[] = new int[]{211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
+        int currentResult=LowestNumber.lowestNumber(array);
+        int ExpectedResult=5;
+        Assert.assertEquals(currentResult,ExpectedResult);
+        System.out.println(" passed");
+    }
+    @Test
+    public void testpattern(){
+
+        ArrayList <Integer> ExpectedResult=PrimeNumber.findPrimes(2,1000);
+        ArrayList<Integer> currentResult= new ArrayList<>();
+        for (int i=2;i<=1000;i++){
+            currentResult.add(i);
+        }
+        Assert.assertEquals(currentResult,ExpectedResult);
+        System.out.println(" passed");
+    }
+}
 
 //Apply Unit testing into each classes and methods in this package.
